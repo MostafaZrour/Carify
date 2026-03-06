0a8619cb21e55b035d5b94a6af99f9b713e0b7c2
Mostafa Zrour <moustazro123@gmail.com>
2026-03-06 00:19:42 +0000

    feat: Add database factories for all models

---
+++ database/factories/CarImageFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Car;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CarImage>
+ */
+class CarImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'car_id' => 1, // We have to provide for which car we are creating an image during creation process
+            'image_path' => function(array $attributes) {
+                $car = Car::find($attributes['car_id']);
+                return sprintf('https://placehold.co/600x400/gray/white/pg?text=%s', $car->maker->name);
+            },
+            'position' => function (array $attributes) {
+                return Car::find($attributes['car_id'])->images()->count() + 1;
+            }
+        ];
+    }
+}
